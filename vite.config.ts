ffcff9f448d6ece82e455ded6e60508048279aea
aqizakana <archifish56@gmail.com>
2026-02-12 00:59:16 +0900

    update: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-
+  base: '/brand-test/',
   plugins: [react()],
   build: {
-    outDir: 'docs'
+    outDir: "docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]"
+      }
+    }
   }
 })
